abf2c13e07296bf59934b69503dc4b9dcddc70df
yshye <yshye824252187@163.com>
2020-03-29 20:44:40 +0800

    bloc_provider

---
+++ lib/bloc/bloc_provider.dart
@@ -0,0 +1,42 @@
+// 所有 BLoCs 的通用接口
+import 'package:flutter/material.dart';
+
+abstract class BlocBase {
+  void dispose();
+}
+
+class BlocProvider<T extends BlocBase> extends StatefulWidget {
+  final T bloc;
+  final Widget child;
+  // @required
+  BlocProvider({
+    Key key,
+    this.child,
+    this.bloc,
+  }) : super(key: key);
+
+
+  @override
+  _BlocProviderState<T> createState() => _BlocProviderState<T>();
+
+  static T of<T extends BlocBase>(BuildContext context) {
+    final type = _typeOf<BlocProvider<T>>();
+    BlocProvider<T> provider = context.ancestorWidgetOfExactType(type);
+    return provider.bloc;
+  }
+
+  static Type _typeOf<T>() => T;
+}
+
+class _BlocProviderState<T> extends State<BlocProvider<BlocBase>> {
+  @override
+  void dispose() {
+    widget.bloc.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.child;
+  }
+}
