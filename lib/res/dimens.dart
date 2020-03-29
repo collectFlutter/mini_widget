152f7381090c8b508e2687103f30a253eab4131f
yshye <yshye824252187@163.com>
2020-03-29 20:46:21 +0800

    res

---
+++ lib/res/dimens.dart
@@ -0,0 +1,8 @@
+class MiniDimen {
+  static const double fontMini = 10;
+  static const double fontSmall = 12;
+  static const double fontNormal = 14;
+  static const double fontLarge = 16;
+  static const double fontXLarge = 18;
+  static const double fontXXLarge = 20;
+}
---
+++ lib/res/styles.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/widgets.dart';
+
+import 'dimens.dart';
+import 'colors.dart';
+
+class MiniStyle {
+  static const TextStyle textTitle =
+      TextStyle(fontSize: MiniDimen.fontLarge, color: MiniColor.textDark, fontWeight: FontWeight.bold);
+  static const TextStyle textNormal = TextStyle(fontSize: MiniDimen.fontNormal, color: MiniColor.textNormal);
+  static const TextStyle textTag = TextStyle(fontSize: MiniDimen.fontSmall, color: MiniColor.textGray);
+  static const TextStyle textUrl = TextStyle(fontSize: MiniDimen.fontNormal, color: MiniColor.blue);
+
+  static const EdgeInsetsGeometry tagPadding = EdgeInsets.only(top: 5, bottom: 5);
+  static const EdgeInsetsGeometry conPadding = EdgeInsets.all(10);
+  static const TextStyle headStyle = TextStyle(fontSize: MiniDimen.fontLarge);
+  static const TextStyle textGrayCNormal = TextStyle(fontSize: MiniDimen.fontNormal, color: MiniColor.textGrayC);
+  static const TextStyle textDarkNormal = TextStyle(fontSize: MiniDimen.fontNormal, color: MiniColor.textDark);
+}
